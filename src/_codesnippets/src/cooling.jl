aa1845e53388b07bffcc010b21201e13030e7ebf
SundaraRaman R <digital_carver@proton.me>
2025-03-08 04:47:26 +0530

    Isolate plotting codesnippets using let blocks
    Avoids unwanted interactions between different code snippets, and
    avoids encouraging writing code in global scope which would be a
    bad Julia practice.

--- src/_codesnippets/src/cooling.jl
+++ src/_codesnippets/src/cooling.jl
@@ -1,25 +1,26 @@
 using QuantumOptics
-bc = FockBasis(16)
-ba = SpinBasis(1//2)
-a = destroy(bc) ⊗ one(ba)
-sm = one(bc) ⊗ sigmam(ba)
-H0 = 0.5*sm'*sm + a + a'
-Hx = 0.5*(a'*sm + a*sm')
-J = [a, sqrt(2)*sm]
-Jdagger = dagger.(J)
 
-fquantum(t, ψ, u) = H0 + Hx*cos(u[1]), J, Jdagger
-function fclassical(du, u, ψ, t)
-  du[1] = 0.3*real(u[2])
-  du[2] = sin(real(u[1]))*real(expect(dagger(a)*sm, ψ))
+let bc = FockBasis(16), ba = SpinBasis(1 // 2)
+  a = destroy(bc) ⊗ one(ba)
+  sm = one(bc) ⊗ sigmam(ba)
+  H0 = 0.5 * sm' * sm + a + a'
+  Hx = 0.5 * (a' * sm + a * sm')
+  J = [a, sqrt(2) * sm]
+  Jdagger = dagger.(J)
+
+  fquantum(t, ψ, u) = H0 + Hx * cos(u[1]), J, Jdagger
+  function fclassical(du, u, ψ, t)
+    du[1] = 0.3 * real(u[2])
+    du[2] = sin(real(u[1])) * real(expect(dagger(a) * sm, ψ))
+  end
+  u0 = ComplexF64[sqrt(2), 6.0]
+  ψ0 = semiclassical.State(fockstate(bc, 0) ⊗ spindown(ba), u0)
+  tout, p = semiclassical.master_dynamic([0:1.0:200;], ψ0, fquantum,
+    fclassical; fout=(t, rho) -> rho.classical[2])
+
+  using PyPlot
+  plot(tout, p .^ 2)
+  ylabel("Kinetic energy")
+  xlabel("Time")
+  savefig("cooling.svg")
 end
-u0 = ComplexF64[sqrt(2), 6.0]
-ψ0 = semiclassical.State(fockstate(bc, 0)⊗spindown(ba), u0)
-tout, p = semiclassical.master_dynamic([0:1.0:200;], ψ0, fquantum,
-      fclassical; fout=(t,rho)->rho.classical[2])
-
-using PyPlot
-plot(tout, p.^2)
-ylabel("Kinetic energy")
-xlabel("Time")
-savefig("cooling.svg")
--- src/_codesnippets/src/gross-pitaevskii.jl
+++ src/_codesnippets/src/gross-pitaevskii.jl
@@ -1,23 +1,24 @@
 using QuantumOptics
-b_x = PositionBasis(-10, 10, 300)
-b_p = MomentumBasis(b_x)
-Tpx = transform(b_p, b_x)
-Txp = transform(b_x, b_p)
-x = position(b_x)
-p = momentum(b_p)
 
-Hkin = LazyProduct(Txp, p^2/2, Tpx)
-Hpsi = diagonaloperator(b_x, Ket(b_x).data)
-H = LazySum(Hkin, Hpsi)
+let b_x = PositionBasis(-10, 10, 300), b_p = MomentumBasis(b_x)
+    Tpx = transform(b_p, b_x)
+    Txp = transform(b_x, b_p)
+    x = position(b_x)
+    p = momentum(b_p)
 
-fquantum(t, ψ) = (Hpsi.data.nzval .= -50 .* abs2.(ψ.data); H)
-ψ₀ = gaussianstate(b_x,-2π,2,1.5) + gaussianstate(b_x,2π,-2,1.5)
-normalize!(ψ₀)
-tout, ψₜ = timeevolution.schroedinger_dynamic([0:0.01:6;], ψ₀, fquantum)
-density = [abs2(ψ.data[j]) for ψ=ψₜ, j=1:length(b_x)]
+    Hkin = LazyProduct(Txp, p^2 / 2, Tpx)
+    Hpsi = diagonaloperator(b_x, Ket(b_x).data)
+    H = LazySum(Hkin, Hpsi)
 
-using PyPlot
-contourf(samplepoints(b_x),tout,density,50)
-xlabel("x")
-ylabel("Time")
-savefig("gross_pitaevskii.png")
+    fquantum(t, ψ) = (Hpsi.data.nzval .= -50 .* abs2.(ψ.data); H)
+    ψ₀ = gaussianstate(b_x, -2π, 2, 1.5) + gaussianstate(b_x, 2π, -2, 1.5)
+    normalize!(ψ₀)
+    tout, ψₜ = timeevolution.schroedinger_dynamic([0:0.01:6;], ψ₀, fquantum)
+    density = [abs2(ψ.data[j]) for ψ = ψₜ, j = 1:length(b_x)]
+
+    using PyPlot
+    contourf(samplepoints(b_x), tout, density, 50)
+    xlabel("x")
+    ylabel("Time")
+    savefig("gross_pitaevskii.png")
+end
